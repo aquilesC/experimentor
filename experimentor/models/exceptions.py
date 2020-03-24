69adcb33ae3628cf9b2cacf62169bbbf28c55f9f
Aquiles Carattino <aquiles@uetke.com>
2020-03-24 16:32:08 +0100

    Defined exceptions for different parts of the program

--- experimentor/models/exceptions.py
+++ experimentor/models/exceptions.py
@@ -1,2 +1,19 @@
-class ModelException(Exception):
+class ExperimentorException(Exception):
+    """Base exception for all experimentor modules"""
+    pass
+
+
+class ModelException(ExperimentorException):
+    pass
+
+
+class PropertyException(ModelException):
+    pass
+
+
+class LinkException(PropertyException):
+    pass
+
+
+class SignalException(ExperimentorException):
     pass
