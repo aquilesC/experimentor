3523576217c7b5e6180e2bf599d044caf7b4366e
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-03 17:10:57 +0200

    Added default finalize. Added publisher creation in meta class

--- experimentor/models/models.py
+++ experimentor/models/models.py
@@ -5,11 +5,20 @@ Models are a buffer between user interactions and real devices. Models should de
 properties, for example how to read a value from a sensor and how to apply a value to an actuator.
 Models can also take care of manipulating data, for example calculating an FFT and returning it to the user.
 """
+import atexit
+import multiprocessing as mp
 import warnings
 import weakref
+from abc import abstractmethod
 
+from experimentor.config import settings
 from experimentor.core.exceptions import ModelDefinitionException
+from experimentor.core.publisher import start_publisher
 from experimentor.core.signal import Signal
+from experimentor.lib.log import get_logger
+
+
+logger = get_logger(__name__)
 
 
 class MetaModel(type):
@@ -27,6 +36,11 @@ class MetaModel(type):
     def __init__(cls, name, bases, attrs):
         # Create class
         super(MetaModel, cls).__init__(name, bases, attrs)
+
+        # Check whether the publisher exists or instantiate it and append it to the class
+        if not hasattr(settings, 'publisher'):
+            logger.info("Publisher not yet initialized. Initializing it")
+            start_publisher()
 
         if {'_signals', 'signals'} & set(attrs):
             raise ModelDefinitionException('Experiments should not define the _signals nor signals attribute themselves')
@@ -97,7 +111,65 @@ class BaseModel(metaclass=MetaModel):
     """All models should inherit from this base model. It defines some basic methods and checks that prevent errors
     later at runtime.
     """
-    pass
+    def __init__(self):
+        atexit.register(self.finalize)
+        self._threads = []
+
+    def clean_up_threads(self):
+        """ Keep only the threads that are alive.
+        """
+        self._threads = [thread for thread in self._threads if thread[1].is_alive()]
+
+    @classmethod
+    def as_process(cls, *args, **kwargs):
+        return ProxyObject(cls, *args, **kwargs)
+
+    @abstractmethod
+    def finalize(self):
+        pass
+
+
+class ProxyObject:
+    def __init__(self, cls, *args, **kwargs):
+        self.parent_pipe, child_pipe = mp.Pipe()
+        if args:
+            args = (cls, child_pipe, args)
+        else:
+            args = (cls, child_pipe)
+        self.child_process = mp.Process(target=_create_process_loop, args=args, kwargs=kwargs)
+        self.child_process.start()
+        print(self.parent_pipe.recv())
+
+        atexit.register(lambda: self.parent_pipe.send(None))
+
+
+def _create_process_loop(cls, command_pipe, *args, **kwargs):
+    if args:
+        if kwargs:
+            obj = cls(args, kwargs)
+        else:
+            obj = cls(kwargs)
+    elif kwargs:
+        obj = cls(kwargs)
+    else:
+        obj = cls()
+
+    command_pipe.send('Instantiated')
+    while True:
+        try:
+            cmd = command_pipe.recv()
+        except EOFError:  # This implies the parent is dead; exit.
+            return None
+        if cmd is None: # This is how the parent signals us to exit.
+            print('Exiting')
+            return None
+        attr_name, args, kwargs = cmd
+        result = getattr(obj, attr_name)(*args, **kwargs)
+        if callable(result):
+            result = lambda: None  # Cheaper than sending a real callable
+        command_pipe.send((result))
+
+
 
 
 class MetaDevice(MetaModel):
@@ -117,17 +189,9 @@ class MetaDevice(MetaModel):
         cls._devices.add(inst)
         return inst
 
+
 class ModelDevice(BaseModel, metaclass=MetaDevice):
     """ All models for devices should inherit from this class.
     """
-    pass
-
-
-class MetaExperiment(MetaModel):
-    pass
-
-
-class ModelExperiment(BaseModel, metaclass=MetaExperiment):
-    """ All experiments should inherit from this class.
-    """
-    pass
+    def __init__(self):
+        super().__init__()
