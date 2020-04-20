0f12e14913eeaae99432269ba1590e7c442176f2
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-20 12:43:28 +0200

    Updated config structure

--- experimentor/config/__init__.py
+++ experimentor/config/__init__.py
@@ -1,18 +1,37 @@
 """
     Settings
     ========
-    Core classes and functions to handle configurations in an Experimentor project. There is still a lot of work to do
-    to make it more robust and extensible, especially when overriding values given by default in the application.
+    Experimentor relies on some general settings in order to run. For example, one can specify the port at which the
+    publisher or pusher connects, or the window which is the starting point for the user interface. We specify some
+    global parameres at :mod:`experimentor.config.global_settings`, that can be overriden at runtime by specifying the
+    environmental variable `EXPERIMENTOR_SETTINGS_MODULE`.
 
+    Only variables written in ALL CAPITAL LETTERS will be taken into account.
+
+    .. note::
+        The inspiration for this flow comes from `Django's Settings module<https://github.com/django/django/blob/c574bec0929cd2527268c96a492d25223a9fd576/django/conf/__init__.py>`_
 """
+
+import importlib
+import os
+
 from experimentor.config import global_settings
 
 
 class Settings:
-    def __init__(self):
+    """ Loads the global parameters and overrides them with those specified in the settings module of the project.
+    """
+    def __init__(self, settings_module):
         for setting in dir(global_settings):
             if setting.isupper():
                 setattr(self, setting, getattr(global_settings, setting))
 
+        self.SETTINGS_MODULE = settings_module
 
-settings = Settings()
+        modifications = importlib.import_module(self.SETTINGS_MODULE)
+        for setting in dir(modifications):
+            if setting.isupper():
+                setattr(self, setting, getattr(modifications, setting))
+
+
+settings = Settings(os.environ.get('EXPERIMENTOR_SETTINGS_MODULE', 'experimentor.config.global_settings'))
--- experimentor/config/global_settings.py
+++ experimentor/config/global_settings.py
@@ -3,6 +3,13 @@ from multiprocessing import Event
 PUBLISHER_PUBLISH_PORT = 5556
 PUBLISHER_PULL_PORT = 5557
 
+EXPERIMENT_MODEL = 'experimentor.models.experiments.BaseExperiment'
+EXPERIMENT_MODEL_INIT = {
+    'config_file': 'config.yml',
+}
+
+START_WINDOW = 'experimentor.views.DataViewWidget'
+
 PUBLISHER_EXIT_KEYWORD = 'stop'
 SUBSCRIBER_EXIT_KEYWORD = 'stop'
 
