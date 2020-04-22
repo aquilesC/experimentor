3feafec3f62bc99e885d88e3df87b48da41915ac
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-22 17:45:59 +0200

    By default experiment model takes no arguments

--- experimentor/config/global_settings.py
+++ experimentor/config/global_settings.py
@@ -17,10 +17,11 @@ from multiprocessing import Event
 PUBLISHER_PUBLISH_PORT = 5556
 PUBLISHER_PULL_PORT = 5557
 
-EXPERIMENT_MODEL = 'experimentor.models.experiments.BaseExperiment'
-EXPERIMENT_MODEL_INIT = {
-    'config_file': 'config.yml',
-}
+EXPERIMENT_MODEL = 'experimentor.models.experiments.Experiment'
+EXPERIMENT_MODEL_INIT = None
+# {
+#     'config_file': 'config.yml',
+# }
 
 START_WINDOW = 'experimentor.views.DataViewWidget'
 
@@ -29,4 +30,4 @@ SUBSCRIBER_EXIT_KEYWORD = 'stop'
 
 GENERAL_STOP_EVENT = Event()
 
-PUBLISHER_READY = False
+PUBLISHER_READY = True
