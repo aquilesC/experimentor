9b6179c047684f5e28c5632423bcab2e206ec4a7
Aquiles Carattino <aqui.carattino@gmail.com>
2020-05-25 17:16:08 +0200

    Changed name of basler.py

--- examples/test_zmq_throughput.py
+++ examples/test_zmq_throughput.py
@@ -4,9 +4,9 @@ from experimentor import Q_
 from experimentor.config import settings
 from experimentor.core.publisher import Publisher, Listener
 from experimentor.core.subscriber import Subscriber
-from experimentor.models.cameras.basler import Camera
+from experimentor.models.cameras.basler import BaslerCamera
 
-cam = Camera('da')
+cam = BaslerCamera('da')
 cam.initialize()
 cam.clear_ROI()
 cam.set_acquisition_mode(cam.MODE_CONTINUOUS)
