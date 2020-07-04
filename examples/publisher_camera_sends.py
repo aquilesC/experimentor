a2536988e2d84d537ff1a168db0b8fc98a445242
Aquiles Carattino <aqui.carattino@gmail.com>
2020-07-04 09:05:21 +0200

    Zero copy zmq

--- examples/publisher_camera_sends.py
+++ examples/publisher_camera_sends.py
@@ -14,7 +14,7 @@ i = 0
 while True:
     try:
         cam.trigger_camera()
-        ans = cam.read_camera()
+        ans = cam.read_camera()[0]
         publisher.send(ans, 0, copy=True, track=False)
         i+=1
         print(f'Sent {i} frames', end='\r')
