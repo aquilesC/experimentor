48589c92817c64e286a5355476a654892101587b
Aquiles Carattino <aqui.carattino@gmail.com>
2020-07-04 09:17:55 +0200

    added fps

--- examples/pull_camera_receive.py
+++ examples/pull_camera_receive.py
@@ -1,12 +1,15 @@
+from time import perf_counter
+
 import zmq
 
 ctx = zmq.Context()
 pull = ctx.socket(zmq.PULL)
-pull.bind('tcp://192.168.0.100:1234')
+pull.connect('tcp://192.168.0.100:1234')
 
 
 i = 0
+t0 = perf_counter()
 while True:
     i += 1
     data = pull.recv(flags=0, copy=True, track=False)
-    print(f'Got {i} frames', end='\r')
+    print(f'Got {i} frames, {i/(perf_counter()-t0)} fps', end='\r')
--- examples/push_camera_sends.py
+++ examples/push_camera_sends.py
@@ -1,3 +1,5 @@
+from time import perf_counter
+
 import zmq
 
 from experimentor.models.devices.cameras.basler.basler import BaslerCamera
@@ -10,14 +12,15 @@ context = zmq.Context()
 pusher = context.socket(zmq.PUSH)
 pusher.bind(f"tcp://*:1234")
 
+t0 = perf_counter()
 i = 0
 while True:
     try:
         cam.trigger_camera()
         ans = cam.read_camera()[0]
         pusher.send(ans, 0, copy=True, track=False)
-        i+=1
-        print(f'Sent {i} frames', end='\r')
+        i += 1
+        print(f'Sent {i} frames, {i/(perf_counter()-t0)} fps', end='\r')
     except KeyboardInterrupt:
         break
 
