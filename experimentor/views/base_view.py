e8f36c53445ab25fe16330037b3f21f22cda94ad
Aquiles Carattino <aqui.carattino@gmail.com>
2020-09-15 16:50:19 +0200

    Added connect to action method

---
+++ experimentor/views/base_view.py
@@ -0,0 +1,29 @@
+"""
+Base View
+=========
+All views defined in Experimentor should inherit from the ``BaseView`` class. This will homogenize the available
+methods that make sense from within the framework. The core idea of the Base View is to be pure-Python, without
+inheriting from any other Qt class. In this way, the BaseView can be used by any kind of view, either main windows,
+widgets, dialogs, etc.
+"""
+
+from logging import warning
+
+
+from experimentor.models.action import Action
+from experimentor.views import try_except_dialog
+
+
+class BaseView:
+    """ Base class to homogenize what views are expected to do through the entire program cycle.
+    """
+    def connect_to_action(self, signal, action):
+        if not isinstance(action, Action):
+            warning(f'{action} is not an Action, consider using {signal}.connect instead')
+
+        @try_except_dialog
+        def handle_action(*args, **kwargs):
+            action()
+
+        signal.connect(handle_action)
+
