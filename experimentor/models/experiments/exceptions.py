3cad0633c5ab57a936780ba255d9f57021e0c4b1
Aquiles Carattino <aqui.carattino@gmail.com>
2021-07-06 17:33:33 +0200

    Added experiment exception

---
+++ experimentor/models/experiments/exceptions.py
@@ -0,0 +1,11 @@
+# ##############################################################################
+#  Copyright (c) 2021 Aquiles Carattino, Dispertech B.V.                       #
+#  exceptions.py is part of experimentor                                      #
+#  This file is released under an MIT license.                                 #
+#  See LICENSE.MD for more information.                                        #
+# ##############################################################################
+from experimentor.models.exceptions import ModelException
+
+
+class ExperimentException(ModelException):
+    pass
