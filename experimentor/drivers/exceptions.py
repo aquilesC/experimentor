63efd40e56099c7080cd35347472dd09412fb92b
Aquiles Carattino <aqui.carattino@gmail.com>
2021-07-16 15:35:48 +0200

    Added general exceptions to drivers

---
+++ experimentor/drivers/exceptions.py
@@ -0,0 +1,11 @@
+# ##############################################################################
+#  Copyright (c) 2021 Aquiles Carattino, Dispertech B.V.                       #
+#  exceptions.py is part of Experimentor.                                      #
+#  This file is released under an MIT license.                                 #
+#  See LICENSE.MD for more information.                                        #
+# ##############################################################################
+from experimentor.core.exceptions import ExperimentorException
+
+
+class DriverException(ExperimentorException):
+    pass
